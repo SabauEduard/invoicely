0037cc3a7f60f8a608ebf2d0d481a155052b45e4
Clem Daria-Ștefana <stefana.daria.sd@gmail.com>
2025-01-11 03:10:40 +0200

    Initialize client with Next.js setup and Tailwind CSS configuration
    Co-authored-by: Maftei Valentin - Vasile <ValentinMaftei@users.noreply.github.com>

---
+++ client/src/app/components/single_home_card.js
@@ -0,0 +1,29 @@
+'use client';
+
+import Image from 'next/image'
+
+
+
+
+export default function SingleHomeCard() {
+    return (
+        <div className='rounded-2xl border-2 items-center flex bg-[#f2f7f8] space-x-5 border-gray-100 w-full h-48'>
+            <Image
+                src="/payment_sticker.png"
+                width={150}
+                height={150}
+                alt="Payment Sticker"
+                className='ml-2'
+            />
+            <div>
+                <div className='flex flex-row items-baseline space-x-2'>
+                    <p className='text-black text-3xl font-bold'>05</p>
+                    <p className='text-black font-bold'>invoices</p>
+                </div>
+                <div>
+                    <p className='text-gray-400 font-medium'>Number of total stored</p>
+                </div>
+            </div>
+        </div>
+    );
+}
