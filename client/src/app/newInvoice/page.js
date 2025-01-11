5b93a792058a408fb74526bb903ad73f468cb292
Clem Daria-Ștefana <stefana.daria.sd@gmail.com>
2025-01-11 18:23:15 +0200

    Add navigation to new invoice page and file input
    Co-authored-by: Maftei Valentin - Vasile <ValentinMaftei@users.noreply.github.com>

--- client/src/app/newInvoice/page.js
+++ client/src/app/newInvoice/page.js
@@ -8,6 +8,7 @@ import {
     Autocomplete,
     AutocompleteItem,
     DatePicker,
+    Chip
 } from "@nextui-org/react";
 import React, { useState } from 'react';
 
@@ -35,18 +36,34 @@ export default function newInvoice() {
     };
 
     const [options, setOptions] = useState([
-        { label: "apartament", key: "apartament"},
-        { label: "utilitati", key: "utilitati"},
-        { label: "casa", key: "casa"},
-        { label: "laptop", key: "laptop"},
+        { label: "apartament", key: "apartament" },
+        { label: "utilitati", key: "utilitati" },
+        { label: "casa", key: "casa" },
+        { label: "laptop", key: "laptop" },
     ]);
-    const [selectedValue, setSelectedValue] = useState(null);
+    const [selectedTags, setSelectedTags] = useState([]);
 
     const handleSelectionChange = (selectedKey) => {
-        setOptions((prevOptions) =>
-          prevOptions.filter((option) => option.key !== selectedKey)
-        );
-      };
+        if (selectedKey !== null) {
+            selectedTags.push(selectedKey);
+            setOptions((prevOptions) =>
+                prevOptions.filter((option) => option.key !== selectedKey)
+            );
+        }
+    };
+
+    const handleDeleteTag = (selectedTag) => {
+        if (selectedTag !== null) { }
+        setSelectedTags((prevTags) => prevTags.filter((tag) => tag !== selectedTag));
+
+        setOptions((prevOptions) => [
+            ...prevOptions,
+            { label: selectedTag, key: selectedTag },
+        ]); 
+
+        console.log("Selected Tags:", selectedTags);
+        console.log(options)
+    }
 
     return (
         <div className='bg-neutral-100 min-h-screen flex flex-col'>
@@ -59,50 +76,80 @@ export default function newInvoice() {
                 <div className="w-full flex items-center justify-between">
                     <h2 className='text-black text-xl font-semibold'>Create New Invoice</h2>
                 </div>
-                <div className='w-[40%] border-2 border-neutral-100 p-6 flex items-center rounded-2xl'>
-                    <Form className='w-full space-y-8' validationBehavior='native' onSubmit={onSubmit}>
-                        <div className='w-full space-y-4'>
-                            <h1 className='text-gray-500 font-medium text-base'>INVOICE DETAILS</h1>
-                            <div className='space-y-5 w-full'>
-                                <div className="flex w-full md:flex-nowrap mb-6 md:mb-0 gap-4">
-                                    <Input isRequired radius='lg' name='vendor' className='w-full' size='sm' label="Vendor" type='text' />
-                                    <Input isRequired radius='lg' name='amount' className='w-full' size='sm' label="Amount (RON)" type='number' />
+                <div className='border-2 border-neutral-100 p-6 flex items-center rounded-2xl'>
+                    <Form className="w-full" validationBehavior='native' onSubmit={onSubmit}>
+                        <div className='w-full h-full flex gap-20'>
+                            <div className="flex flex-col flex-1 items-end space-y-6">
+                                <div className='w-full space-y-4'>
+                                    <h1 className='text-gray-500 font-medium text-base'>INVOICE DETAILS</h1>
+                                    <div className='space-y-5 w-full'>
+                                        <div className="flex w-full md:flex-nowrap mb-6 md:mb-0 gap-4">
+                                            <Input isRequired radius='lg' name='vendor' className='w-full' size='sm' label="Vendor" type='text' />
+                                            <Input isRequired radius='lg' name='amount' className='w-full' size='sm' label="Amount (RON)" type='number' />
+                                        </div>
+                                        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                                            <Autocomplete radius='lg' className="w-full" name='importance' size='sm' isRequired label="Importance">
+                                                {importance.map((importance) => (
+                                                    <AutocompleteItem key={importance.key}>{importance.label}</AutocompleteItem>
+                                                ))}
+                                            </Autocomplete>
+                                            <Autocomplete radius='lg' className="w-full" name='status' size='sm' isRequired label="Status">
+                                                {status.map((status) => (
+                                                    <AutocompleteItem key={status.key}>{status.label}</AutocompleteItem>
+                                                ))}
+                                            </Autocomplete>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                                    <Autocomplete radius='lg' className="w-full" name='importance' size='sm' isRequired label="Importance">
-                                        {importance.map((importance) => (
-                                            <AutocompleteItem key={importance.key}>{importance.label}</AutocompleteItem>
-                                        ))}
-                                    </Autocomplete>
-                                    <Autocomplete radius='lg' className="w-full" name='status' size='sm' isRequired label="Status">
-                                        {status.map((status) => (
-                                            <AutocompleteItem key={status.key}>{status.label}</AutocompleteItem>
-                                        ))}
-                                    </Autocomplete>
+                                <div className='w-full space-y-4'>
+                                    <h1 className='text-gray-500 font-medium text-base'>DATES</h1>
+                                    <div className="flex w-full md:flex-nowrap mb-6 md:mb-0 gap-4">
+                                        <DatePicker className='w-full' radius='lg' name='emissionDate' size='sm' isRequired label='Emission Date' />
+                                        <DatePicker className='w-full' radius='lg' name='dueDate' size='sm' isRequired label='Due Date' />
+                                    </div>
+                                </div>
+                                <div className='w-full space-y-4'>
+                                    <h1 className='text-gray-500 font-medium text-base'>DETAILS</h1>
+                                    <div className="flex w-full md:flex-nowrap mb-6 md:mb-0 gap-4">
+                                        <Input radius='lg' name='note' className='w-full' size='sm' label="Note" type='text' />
+                                        <Autocomplete radius='lg' className="w-full" name='tags' size='sm' label="Tags" onSelectionChange={handleSelectionChange}>
+                                            {options.map((option) => (
+                                                <AutocompleteItem key={option.key}>{option.label}</AutocompleteItem>
+                                            ))}
+                                        </Autocomplete>
+                                    </div>
+                                </div>
+                                <div className='w-full'>
+                                    {
+                                        selectedTags.length > 0 && (
+                                            <div className="flex flex-wrap gap-2">
+                                                {selectedTags.map((tag) => (
+                                                    <Chip key={tag} variant="flat" onClose={() => handleDeleteTag(tag)}>
+                                                        {tag}
+                                                    </Chip>
+                                                ))}
+                                            </div>
+                                        )
+                                    }
+                                </div>
+                                <Button className='self-end' type="submit" color="primary">
+                                    Submit
+                                </Button>
+                            </div>
+                            <div className='flex-1'>
+                                <div className="flex flex-col h-full">
+                                    <div onDragEnter={() => { }} onDragLeave={() => { }} onDragOver={() => { }} onDrop={() => { }}></div>
+                                    <label className="flex flex-col justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                                        <div className='h-full flex flex-col items-center justify-center py-2'>
+                                            <svg className="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                            </svg>
+                                            <p className="mb-2 text-sm text-gray-500"><span className="font-semibold">Click to upload</span> or drag an invoice</p>
+                                        </div>
+                                    </label>
                                 </div>
                             </div>
                         </div>
-                        <div className='w-full space-y-4'>
-                            <h1 className='text-gray-500 font-medium text-base'>DATES</h1>
-                            <div className="flex w-full md:flex-nowrap mb-6 md:mb-0 gap-4">
-                                <DatePicker className='w-full' radius='lg' name='emissionDate' size='sm' isRequired label='Emission Date' />
-                                <DatePicker className='w-full' radius='lg' name='dueDate' size='sm' isRequired label='Due Date' />
-                            </div>
-                        </div>
-                        <div className='w-full space-y-4'>
-                            <h1 className='text-gray-500 font-medium text-base'>DETAILS</h1>
-                            <div className="flex w-full md:flex-nowrap mb-6 md:mb-0 gap-4">
-                                <Input radius='lg' name='note' className='w-full' size='sm' label="Note" type='text' />
-                                <Autocomplete radius='lg' className="w-full" name='tags' size='sm' label="Tags" onSelectionChange={handleSelectionChange}>
-                                    {options.map((option) => (
-                                        <AutocompleteItem key={option.key}>{option.label}</AutocompleteItem>
-                                    ))}
-                                </Autocomplete>
-                            </div>
-                        </div>
-                        <Button className='self-end' type="submit" color="primary">
-                            Submit
-                        </Button>
                     </Form>
                 </div>
             </div>
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -3,37 +3,39 @@
 import Logo from './components/logo.js';
 import HomeCards from './components/home_cards.js';
 import SingleHomeCard from './components/single_home_card.js';
-import { Button, Input } from "@nextui-org/react";
-import Search from './components/search.js';
+import { Button } from "@nextui-org/react";
 import TableContent from './components/tableContent.js';
+import { useRouter } from 'next/navigation';
 
 export const PlusIcon = ({ size = 24, width, height, ...props }) => {
   return (
-      <svg
-          aria-hidden="true"
-          fill="none"
-          focusable="false"
-          height={size || height}
-          role="presentation"
-          viewBox="0 0 24 24"
-          width={size || width}
-          {...props}
+    <svg
+      aria-hidden="true"
+      fill="none"
+      focusable="false"
+      height={size || height}
+      role="presentation"
+      viewBox="0 0 24 24"
+      width={size || width}
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
       >
-          <g
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={1.5}
-          >
-              <path d="M6 12h12" />
-              <path d="M12 18V6" />
-          </g>
-      </svg>
+        <path d="M6 12h12" />
+        <path d="M12 18V6" />
+      </g>
+    </svg>
   );
 };
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className='bg-neutral-100 min-h-screen flex flex-col'>
       <div className="flex items-center justify-between w-full h-20 px-6">
@@ -44,7 +46,7 @@ export default function Home() {
         className="bg-white px-10 py-8 rounded-l-3xl ml-20 mb-10 space-y-8">
         <div className="w-full flex items-center justify-between">
           <h2 className='text-black text-xl font-semibold'>Invoices</h2>
-          <Button color="primary" startContent={<PlusIcon />}>Add new invoice</Button>
+          <Button color="primary" startContent={<PlusIcon />} onPress={() => router.push('/newInvoice')}>Add new invoice</Button>
         </div>
         <div className='space-x-5 flex flex-row w-full justify-center'>
           <HomeCards />
