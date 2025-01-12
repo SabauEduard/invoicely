c81e0c0c4eb794f434224cb7547e781999571bd9
MafteiValentin <maftei_valentin06@yahoo.com>
2025-01-12 23:20:22 +0200

    added fe

---
+++ client/src/app/context/AuthProvider.js
@@ -0,0 +1,57 @@
+'use client';
+
+import { createContext, use, useState, useContext } from "react";
+import api from "../api/api";
+
+const AuthContext = createContext({});
+const LOGIN_URL = '/auth/login';
+const ME_URL = '/auth/current-user';
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(() => {
+        const user = localStorage.getItem('user');
+        return user ? JSON.parse(user) : null;
+    });
+
+    const loginApiCall = async (payload) => {
+        try {
+            const response = await api.post(LOGIN_URL, payload,
+                {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    withCredentials: true,
+                });
+
+            const token = response.data.access_token;
+
+            const user_response = await api.get(ME_URL,
+                {
+                    withCredentials: true,
+                });
+
+            setUser(user_response.data);
+            localStorage.setItem('user', JSON.stringify(user_response.data));
+            return user_response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    const logout = async () => {
+        document.cookie = "auth_cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        localStorage.removeItem('user');
+        localStorage.clear();
+        setUser(null);
+    }
+
+    return (
+        <AuthContext.Provider value={{ loginApiCall, logout, user }}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+export default AuthContext;
+
+export const useAuth = () => useContext(AuthContext);
